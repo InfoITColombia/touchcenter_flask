77a27d2845e5d24aecd3bb007d95deded92853a5
miguellperezzv <42900928+miguellperezzv@users.noreply.github.com>
2023-09-23 19:52:27 -0500

    Aceptar peticiones externas

--- run.py
+++ run.py
@@ -4,4 +4,5 @@ if __name__ == "__main__":
     print("ESTO AQUI ")
     app_flask = create_app()
     print("DEBUG" + str(app_flask.debug))
+    app_flask.run(host='0.0.0.0', port=5000)
     app_flask.run()
